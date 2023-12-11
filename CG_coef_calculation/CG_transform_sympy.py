fixed 2 methods of the calculation of CG coefs

CG_coef_calculation/CG_transform_sympy.py
@@ -6,7 +6,7 @@ import re
 # CG(j1, m1, j2, m2, J, M)
 
 
-def vec_pos(j1, j2, m1, m2):
+def vec_pos(j1, m1, j2, m2):
     id1 = (j1 - m1)
     id2 = (j2 - m2)
     return int(id1 * (2 * j2 + 1) + id2)
@@ -23,17 +23,29 @@ def uni_array(start, end, step):
     return np.arange(start, end + step, step)
 
 
+def is_numeric_string(s):
+    print(s)
+    return (s[0] == '-' and s[1:].isdigit()) or s.isdigit()
+
+
 def CGtrans(j1, j2):
 
-    mat = np.zeros((2 * j1 + 1) * (2 * j2 + 1))
+    mat = np.zeros([(int)(2 * j1 + 1) * (int)(2 * j2 + 1),
+                   (int)(2 * j1 + 1) * (int)(2 * j2 + 1)])
     for J in uni_array(j1 + j2, j1 - j2, -1):
         for M in uni_array(J, -J, -1):
             for m1 in uni_array(j1, -j1, -1):
                 m2 = M - m1
                 if (m2 > j2 or m2 < - j2):
                     continue
-                cg = CG(S(j1 * 2) / 2, S(int(m1 * 2)) /
-                        2, S(j2 * 2) / 2, S(m2 * 2) / 2, S(J * 2) / 2, S(M * 2) / 2)
+                cg = CG(j1, m1, j2, m2, J, M)
                 val = cg.doit()
-                match = re.match
-                mat[J_pos(j1 + j2, J, M)][vec_pos(j1, m1, j2, m2)]
+                mat[J_pos(j1 + j2, J, M)][vec_pos(j1,
+                                                  m1, j2, m2)] = val.evalf()
+
+    return mat
+
+
+if __name__ == "__main__":
+    mat = CGtrans(5/2, 3/2)
+    print(mat.round(decimals=2))

CG_coef_calculation/simple_schur_transform.py
@@ -28,10 +28,9 @@ def CG_Coef(j1, j2):
 
     认为 (j1, s1) tensor (j2, s2) 占据的列向量下标是 s1 * (2j2+1) + s2
     '''
-    mat = np.zeros([(j1+1)*(j2+1), (j1+1)*(j2+1)])
 
-    j1 = j1 / 2
-    j2 = j2 / 2
+    mat = np.zeros([(int)(2 * j1 + 1) * (int)(2 * j2 + 1),
+                   (int)(2 * j1 + 1) * (int)(2 * j2 + 1)])
 
     row = 0
 
@@ -81,5 +80,5 @@ def CG_Coef(j1, j2):
 
 if __name__ == "__main__":
     n = 2
-    mat = CG_Coef(3, 1)
-    print(mat[J_pos(2, 1, 1)][vec_pos(3/2, 1/2, 3/2, -1/2)])
+    mat = CG_Coef(5/2, 3/2)
+    print(mat.round(decimals=2))