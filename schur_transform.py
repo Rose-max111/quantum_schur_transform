4bf8da4177f66f0045139ca5f499e43affbc7e74
max Rose <rose@Rose-Mac-mini.local>
2023-12-11 11:15:35 +0800

    building the framework for the schur transform

---
+++ schur_transform.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+class J_invariant_space:
+
+    def __init__(self, J):
+        self.J = J
+
+    def next()
+
+def merge(N, J):
+    '''
+    向 2^N 的 J_invariant_space 中添入一个 spin-1/2 的 space
+    '''
+
+
+if __name__ == "__main__":
+
+    n = 2
+    mat = np.eye(2**n)
+
+
+    for i in range(n-1):
+        mat = merge(i) * mat
